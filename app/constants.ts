d03602c33c2d2f93e8a0bf609f3d8ad60fcf82cc
mariandev520 <manzomariano@hotmail.com>
2022-04-25 15:16:46 -0300

    beta1.1

---
+++ app/constants.ts
@@ -0,0 +1,20 @@
+export const INFORMATION = {
+  avatar: "/assets/avatar.jpg",
+  banner: "/assets/banner.jpg",
+  title: "Mofletesmdp",
+  description:
+    "Ropita para bebés y niños de la mejor calidad! Entregas a domicilio en Mar del Plata",
+  phone: `5491141414141`,
+  sheet: `https://docs.google.com/spreadsheets/d/e/2PACX-1vR6XdrpjQ-zuLmnwNHBQ8NOWoeWBSA-Or77wXix4dmp38pBE5fvVgFhdTxxlAlSM9YezHecRTu3Cd43/pub?output=csv`,
+  color: `purple`,
+  social: [
+    {
+      name: "instagram",
+      url: ``,
+    },
+    {
+      name: "whatsapp",
+      url: `https://wa.me/5491160325056`,
+    },
+  ],
+};
---
+++ theme.ts
@@ -0,0 +1,10 @@
+import {extendTheme, theme} from "@chakra-ui/react";
+
+import {INFORMATION} from "./app/constants";
+
+export default extendTheme({
+  colors: {
+    primary: theme.colors[INFORMATION.color],
+    brand: "#512364",
+  },
+});
